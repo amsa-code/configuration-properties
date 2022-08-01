d69b9fdd01041d3b53c50f278a602022cccccbd6
Dave Moten <davidmoten@gmail.com>
2022-08-01 20:47:22 +1000

    add test resources

--- src/main/java/au/gov/amsa/configuration/properties/AutoClosingInputStreamConfiguration.java
+++ src/main/java/au/gov/amsa/configuration/properties/AutoClosingInputStreamConfiguration.java
@@ -9,7 +9,6 @@ import java.util.Properties;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-
 /**
  * Loads configuration from an InputStream
  * 
@@ -17,56 +16,55 @@ import org.slf4j.LoggerFactory;
  * 
  */
 public class AutoClosingInputStreamConfiguration implements Configuration {
-	private final Properties props;
-	private static Logger log = LoggerFactory
-			.getLogger(AutoClosingInputStreamConfiguration.class);
+    private final Properties props;
+    private static Logger log = LoggerFactory.getLogger(AutoClosingInputStreamConfiguration.class);
 
-	public AutoClosingInputStreamConfiguration(InputStream is) {
-		props = new Properties();
-		try {
-			props.load(is);
-		} catch (IOException e) {
-			throw new UncheckedIOException(e);
-		} finally {
-		    try {
+    public AutoClosingInputStreamConfiguration(InputStream is) {
+        props = new Properties();
+        try {
+            props.load(is);
+        } catch (IOException e) {
+            throw new UncheckedIOException(e);
+        } finally {
+            try {
                 is.close();
             } catch (IOException e) {
                 throw new UncheckedIOException(e);
             }
-		}
-	}
+        }
+    }
 
-	@Override
-	public final String getProperty(String name) {
-		String o = props.getProperty(name);
-		if (o == null)
-			log.info("property " + name + " not found");
-		else {
-			Object value = o;
-			if (name.contains("password"))
-				value = "*****";
-			log.info("property " + name + "=" + value);
-		}
-		return o;
-	}
+    @Override
+    public final String getProperty(String name) {
+        String o = props.getProperty(name);
+        if (o == null)
+            log.info("property " + name + " not found");
+        else {
+            Object value = o;
+            if (name.contains("password"))
+                value = "*****";
+            log.info("property " + name + "=" + value);
+        }
+        return o;
+    }
 
-	@Override
-	public final Enumeration<String> getPropertyNames() {
+    @Override
+    public final Enumeration<String> getPropertyNames() {
 
-		final Enumeration<Object> e = props.keys();
-		return new Enumeration<String>() {
+        final Enumeration<Object> e = props.keys();
+        return new Enumeration<String>() {
 
-			@Override
-			public boolean hasMoreElements() {
-				return e.hasMoreElements();
-			}
+            @Override
+            public boolean hasMoreElements() {
+                return e.hasMoreElements();
+            }
 
-			@Override
-			public String nextElement() {
-				return (String) e.nextElement();
-			}
-		};
+            @Override
+            public String nextElement() {
+                return (String) e.nextElement();
+            }
+        };
 
-	}
+    }
 
 }
